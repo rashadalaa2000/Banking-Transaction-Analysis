f28baf3fbb812d83c68ed0e962c56b74740b6e51
Rashad Alaa <rashadalaa2000@gmail.com>
2026-04-24 08:16:44 +0300

    Update 03_data_cleaning.sql

--- sql-server-data-engineering/03_data_cleaning.sql
+++ sql-server-data-engineering/03_data_cleaning.sql
@@ -1,1 +1,38 @@
+-- Check Medain
+SELECT top 1
+    PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY customer_account_balance) OVER () AS median_cab
+FROM fact_transaction; -- 16792.18 => Medain 
 
+
+-- 1. CustAccountBalance NULLs
+UPDATE fact_transaction
+SET customer_account_balance = 16792.18 -- Medain 
+WHERE customer_account_balance IS NULL;
+
+-- 2. clear unlogical value
+UPDATE dim_customers
+SET 
+    customer_age = NULL,
+    customer_bdate = NULL
+WHERE customer_age < 16 OR customer_age > 100;
+
+
+-- Check
+SELECT
+    SUM(CASE WHEN CustAccountBalance IS NULL THEN 1 ELSE 0 END) AS balance_nulls,
+    SUM(CASE WHEN CustomerDOB IS NULL THEN 1 ELSE 0 END)        AS dob_nulls
+FROM bank_transactions;
+
+
+-- 3. REPLACE T WITH NULL
+UPDATE dim_customers
+SET customer_gender = NULL
+WHERE customer_gender = 'T';
+
+
+-- Check
+SELECT 
+    DISTINCT customer_gender,
+    COUNT(*) AS count_value
+FROM dim_customers
+GROUP BY customer_gender;
